d05aaacd54cda927ab23883e95f9357a6c8e0f47
SpectrumTantrum <119471486+SpectrumTantrum@users.noreply.github.com>
2022-11-30 20:54:44 +0800

    Add files via upload

---
+++ Java/betweenminmax/betweenminmax.java
@@ -0,0 +1,14 @@
+package betweenminmax;
+
+import java.util.Scanner;
+
+public class betweenminmax{
+
+    /**
+* Fill this in with a Javadoc style comment.
+*/
+public boolean inRange(int num, int min, int max){
+    
+}
+    
+}
